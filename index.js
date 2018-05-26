219d7a779fe178e0fac196ad3413506c84360c3e
JulioProfe <julio_15.97@hotmail.com>
2018-05-25 19:18:52 -0500

    Avances

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-var desacuerdo = 0,
-    acuerdo = 0;
+var sumatoriaFajardo = 0;
+var sumatoriaDuque = 0;
+var sumatoriaCalle = 0;
+var sumatoriaLleras = 0;
+var sumatoriaPetro = 0;
+
+var casos = 0;
+var recomendacion = [];
+
+var contenedorFajardo = 0;
+var contenedorLleras = 0;
+var contenedorPetro = 0;
+var contenedorCalle = 0;
+var contenedorDuque = 0;
+
+var afinidadFajardo = [];
+var afinidadLleras = [];
+var afinidadPetro = [];
+var afinidadCalle = [];
+var afinidadDuque = [];
 
 var importancia = [];
+var preguntasTema = [];
 
 var temas = [
     "Salud",
@@ -16,43 +35,309 @@ var temas = [
     "TIC"
 ];
 
+var temasImportantes = [
+    "salud",
+    "social",
+    "acuerdopaz",
+    "educacion",
+    "economia",
+    "medioambiente",
+    "politica",
+    "seguridad",
+    "politico",
+    "tecnologia"
+];
+
 
 var addTemas = function () {
     for (i = 0; i < temas.length; i++) {
         const element = temas[i];
+        //console.log(temas[i]);
+        if(typeof window !== 'undefined') {
         var tema = document.createElement("li");
         tema.innerHTML = "<a href='#' onclick='addImport("+i+")'>"+element+" </a>";
         document.querySelector("#temas .temas ol").appendChild(tema);
+        }
     }
-
 };
 
-function addImport(a) { 
-    importancia.push(temas[a]);
+function addImport(a) {
+    importancia.push(temasImportantes[a]);
     //console.log("TAMAÑO DEL ARRAY "+importancia);
-    
     var tema = document.createElement("li");
     tema.innerHTML = temas[a];
     document.querySelector("#temas .importancia ol").appendChild(tema);
-
-    
-    //console.log("ESTOY SELECCIONANDO " + document.querySelector("#temas .temas ol").getElementsByTagName("li"));
-
+    //console.log("ESTOY SELECCIONANDO " + document.querySelector("#temas .temas ol").getElementsByTagName("li"))
     var ocultar = document.querySelector("#temas .temas ol").getElementsByTagName("li")[a].style.visibility = "hidden";
 
     console.log("ESTOY SELECCIONANDO " + ocultar);
 }
 
 
-function sumarA() {
-    acuerdo++;
-    console.log("Acuerdo: " + acuerdo);
-
+function sumarA(numPregunta, tematica) {
+    evaluarImportanciaAcuerdo(numPregunta, tematica);
+    console.log((contenedorFajardo*100)/sumatoriaFajardo)
 }
 
-function sumarD() {
-    desacuerdo++;
-    console.log("Desacuerdo: " + desacuerdo);
+function sumarD(numPregunta, tematica) {
+    evaluarImportanciaDesacuerdo(numPregunta, tematica);
+}
+
+function hacerSumatorias(){
+    //FAJARDO
+    for (index = 0; index < afinidadFajardo.length; index++) {
+        if (afinidadFajardo[index] >= 2)
+            sumatoriaFajardo += afinidadFajardo[index];
+        /*else if (afinidadFajardo[num] == 1)
+            sumatoriaFajardo += 3;
+        else if (afinidadFajardo[num] == 0)
+            sumatoriaFajardo += 4;*/
+    }
+
+        //DUQUE
+        for (index = 0; index < afinidadDuque.length; index++) {
+            if (afinidadDuque[index] >= 2)
+                sumatoriaDuque += afinidadDuque[index];
+            /*else if (afinidadFajardo[num] == 1)
+                sumatoriaFajardo += 3;
+            else if (afinidadFajardo[num] == 0)
+                sumatoriaFajardo += 4;*/
+        }
+
+        //CALLE
+        for (index = 0; index < afinidadCalle.length; index++) {
+            if (afinidadCalle[index] >= 2)
+                sumatoriaCalle += afinidadDuque[index];
+            /*else if (afinidadFajardo[num] == 1)
+                sumatoriaFajardo += 3;
+            else if (afinidadFajardo[num] == 0)
+                sumatoriaFajardo += 4;*/
+        }
+
+        //LLERAS
+        for (index = 0; index < afinidadLleras.length; index++) {
+            if (afinidadLleras[index] >= 2)
+                sumatoriaLleras += afinidadLleras[index];
+            /*else if (afinidadFajardo[num] == 1)
+                sumatoriaFajardo += 3;
+            else if (afinidadFajardo[num] == 0)
+                sumatoriaFajardo += 4;*/
+        }
+
+        //PETRO
+        for (index = 0; index < afinidadPetro.length; index++) {
+            if (afinidadPetro[index] >= 2)
+                sumatoriaPetro += afinidadPetro[index];
+            /*else if (afinidadFajardo[num] == 1)
+                sumatoriaFajardo += 3;
+            else if (afinidadFajardo[num] == 0)
+                sumatoriaFajardo += 4;*/
+        }
+}
+
+function evaluacionAfinidad(){
+    var crespo = ((contenedorFajardo*100)/sumatoriaFajardo);
+    var street = ((contenedorCalle*100)/sumatoriaCalle);
+    var porky = ((contenedorDuque*100)/sumatoriaDuque);
+    var mocho = ((contenedorLleras*100)/sumatoriaLleras);
+    var petrosky = ((contenedorPetro*100)/sumatoriaPetro); 
+
+    recomendacion.push({
+        nombreC: "Sergio Fajardo",
+        porcentaje: crespo
+    },
+    {
+        nombreC: "Humberto de la Calle",
+        porcentaje: street
+    },
+    {
+        nombreC: "Ivan Duque",
+        porcentaje: porky
+    },
+    {
+        nombreC: "German Vargas Lleras",
+        porcentaje: mocho
+    },
+    {
+        nombreC: "Gustavo Petro",
+        porcentaje: petrosky
+    });
+
+    recomendacion.sort(function(a, b){return b.porcentaje-a.porcentaje});
+    
+    console.log(recomendacion[0]);
+}
+
+function evaluarImportanciaAcuerdo(num, tematica){
+    if(importancia.length == 10){
+    for (index = 0; index < importancia.length; index++) {
+        if (index < 4) {
+            //console.log(importancia[index]);
+            if (importancia[index].trim() === tematica.trim()){
+                contenedorFajardo += afinidadFajardo[num];
+                contenedorCalle += afinidadCalle[num];
+                contenedorDuque += afinidadDuque[num];
+                contenedorLleras += afinidadLleras[num];
+                contenedorPetro += afinidadPetro[num];
+                console.log("FAJARDO: " + contenedorFajardo);
+                console.log("CALLE: " + contenedorCalle);
+                console.log("DUQUE: " + contenedorDuque);
+                console.log("LLERAS: " + contenedorLleras);
+                console.log("PETRO: " + contenedorPetro);
+                }
+        } else if (index >= 4 && index < 8) {
+            if(importancia[index].trim() === tematica.trim()){
+                contenedorFajardo += afinidadFajardo[num]/2;
+                contenedorCalle += afinidadCalle[num]/2;
+                contenedorDuque += afinidadDuque[num]/2;
+                contenedorLleras += afinidadLleras[num]/2;
+                contenedorPetro += afinidadPetro[num]/2;
+                console.log("FAJARDO: " + contenedorFajardo);
+                console.log("CALLE: " + contenedorCalle);
+                console.log("DUQUE: " + contenedorDuque);
+                console.log("LLERAS: " + contenedorLleras);
+                console.log("PETRO: " + contenedorPetro);
+                }
+        } else if (index >= 8) {
+            if(importancia[index].trim() === tematica.trim()){
+                contenedorFajardo += afinidadFajardo[num]/6;
+                contenedorCalle += afinidadCalle[num]/6;
+                contenedorDuque += afinidadDuque[num]/6;
+                contenedorLleras += afinidadLleras[num]/6;
+                contenedorPetro += afinidadPetro[num]/6;
+                console.log("FAJARDO: " + contenedorFajardo);
+                console.log("CALLE: " + contenedorCalle);
+                console.log("DUQUE: " + contenedorDuque);
+                console.log("LLERAS: " + contenedorLleras);
+                console.log("PETRO: " + contenedorPetro);
+                }
+            }   
+        }
+    } 
+    //console.log(importancia.length);
+}
+
+function evaluarImportanciaDesacuerdo(num, tematica){
+    if(importancia.length == 10){
+    for (index = 0; index < importancia.length; index++) {
+        if (index < 4) {
+            if (importancia[index].trim() === tematica.trim()){
+                    //FAJARDO
+                    if (afinidadFajardo[num] == 0) {
+                        contenedorFajardo += 4;
+                    }if (afinidadFajardo[num] == 1) {
+                        contenedorFajardo += 3;
+                    }
+                    //CALLE
+                    if (afinidadCalle[num] == 0) {
+                        contenedorCalle += 4;
+                    }if (afinidadCalle[num] == 1) {
+                        contenedorCalle += 3;
+                    }
+                    //DUQUE
+                    if (afinidadDuque[num] == 0) {
+                        contenedorDuque += 4;
+                    }if (afinidadDuque[num] == 1) {
+                        contenedorDuque += 3;
+                    }
+                    //LLERAS
+                    if (afinidadLleras[num] == 0) {
+                        contenedorLleras += 4;
+                    }if (afinidadLleras[num] == 1) {
+                        contenedorLleras += 3;
+                    }
+                    //PETRO
+                    if (afinidadPetro[num] == 0) {
+                        contenedorPetro += 4;
+                    }if (afinidadPetro[num] == 1) {
+                        contenedorPetro += 3;
+                    }
+                    console.log("FAJARDO: " + contenedorFajardo);
+                    console.log("CALLE: " + contenedorCalle);
+                    console.log("DUQUE: " + contenedorDuque);
+                    console.log("LLERAS: " + contenedorLleras);
+                    console.log("PETRO: " + contenedorPetro);
+                }
+        } else if (index >= 4 && index < 8) {
+            if(importancia[index].trim() === tematica.trim()){
+                    //FAJARDO
+                    if (afinidadFajardo[num] == 0) {
+                        contenedorFajardo += 2;
+                    }if (afinidadFajardo[num] == 1) {
+                        contenedorFajardo += 1.5;
+                    }
+                    //CALLE
+                    if (afinidadCalle[num] == 0) {
+                        contenedorCalle += 2;
+                    }if (afinidadCalle[num] == 1) {
+                        contenedorCalle += 1.5;
+                    }
+                    //DUQUE
+                    if (afinidadDuque[num] == 0) {
+                        contenedorDuque += 2;
+                    }if (afinidadDuque[num] == 1) {
+                        contenedorDuque += 1.5;
+                    }
+                    //LLERAS
+                    if (afinidadLleras[num] == 0) {
+                        contenedorLleras += 2;
+                    }if (afinidadLleras[num] == 1) {
+                        contenedorLleras += 1.5;
+                    }
+                    //PETRO
+                    if (afinidadPetro[num] == 0) {
+                        contenedorPetro += 2;
+                    }if (afinidadPetro[num] == 1) {
+                        contenedorPetro += 1.5;
+                    }
+                    console.log("FAJARDO: " + contenedorFajardo);
+                    console.log("CALLE: " + contenedorCalle);
+                    console.log("DUQUE: " + contenedorDuque);
+                    console.log("LLERAS: " + contenedorLleras);
+                    console.log("PETRO: " + contenedorPetro);
+                }
+            } else if (index >= 8) {
+                if(importancia[index].trim() === tematica.trim()){
+                //FAJARDO
+                if (afinidadFajardo[num] == 0) {
+                    contenedorFajardo += 0.75;
+                }if (afinidadFajardo[num] == 1) {
+                    contenedorFajardo += 0.33;
+                }
+                //CALLE
+                if (afinidadCalle[num] == 0) {
+                    contenedorCalle += 0.75;
+                }if (afinidadCalle[num] == 1) {
+                    contenedorCalle += 0.33;
+                }
+                //DUQUE
+                if (afinidadDuque[num] == 0) {
+                    contenedorDuque += 0.75;
+                }if (afinidadDuque[num] == 1) {
+                    contenedorDuque += 0.33;
+                }
+                //LLERAS
+                if (afinidadLleras[num] == 0) {
+                    contenedorLleras += 0.75;
+                }if (afinidadLleras[num] == 1) {
+                    contenedorLleras += 0.33;
+                }
+                //PETRO
+                if (afinidadPetro[num] == 0) {
+                    contenedorPetro += 0.75;
+                }if (afinidadPetro[num] == 1) {
+                    contenedorPetro += 0.33;
+                }
+                console.log("FAJARDO: " + contenedorFajardo);
+                console.log("CALLE: " + contenedorCalle);
+                console.log("DUQUE: " + contenedorDuque);
+                console.log("LLERAS: " + contenedorLleras);
+                console.log("PETRO: " + contenedorPetro);
+                }
+            }
+        }
+    } 
+    //console.log(importancia.length);
 }
 
 addTemas();
--- static/js/loader.js
+++ static/js/loader.js
@@ -22,46 +22,89 @@ $(document).ready(function () {
                 fajardo: tempData.fajardo,
                 petro: tempData.petro,
                 duque: tempData.duque,
+                tema: tempData.tema
             });
         }
-        console.log(pollQuestionsProcesed);
+        //console.log(pollQuestionsProcesed);
         addToDom();
     });
-
-
 });
-
 
 //Method that add the poll question arrays to the dom (Solo es a modo de ejemplo pues ya teniendo el array de preguntas deben aplicar su propia logica)
 addToDom = function () {
-    console.log("Callme");
-    console.log("Array length: ", pollQuestionsProcesed.length);
+    /*console.log("Callme");
+    console.log("Array length: ", pollQuestionsProcesed.length);*/
     for (var i = pollQuestionsProcesed.length - 1; i >= 0; i--) {
 
-        console.log("Position: ", i);
+        //console.log("Position: ", i);
         var pregunta = pollQuestionsProcesed[i].pregunta;
         var vargas = pollQuestionsProcesed[i].vargas;
         var calle = pollQuestionsProcesed[i].calle;
         var fajardo = pollQuestionsProcesed[i].fajardo;
         var petro = pollQuestionsProcesed[i].petro;
         var duque = pollQuestionsProcesed[i].duque;
+        var tema = pollQuestionsProcesed[i].tema;
+
+        preguntasTema[i] = pollQuestionsProcesed[i].tema;
+
+        afinidadFajardo.push(fajardo);
+        afinidadCalle.push(calle);
+        afinidadDuque.push(duque);
+        afinidadLleras.push(vargas);
+        afinidadPetro.push(petro);
 
         var seccion_pregunta = document.createElement('section');
         seccion_pregunta.className = 'pregunta';
-        seccion_pregunta.innerHTML =
-            "<div class= 'preguntas-container'>" +
-            "<ul><li>Pregunta: " + pregunta + "</li>" +
-            "<li>vargas: " + vargas + "</li>" +
-            "<li>calle: " + calle + "</li>" +
-            "<li>Fajardo: " + fajardo + "</li>" +
-            "<li>Petro: " + petro + "</li>" +
-            "<li>Duque: " + duque + "</li>" +
-            "</ul>" + "</div>" +
-            "<div class= 'buttons-container'>" +
-            "<button onclick= 'sumarD()' class= 'desagree'>Desacuerdo</button>" +
-            "<button onclick= 'sumarA()' class= 'agree'>De acuerdo</button>" +
-           "</div>";
+        seccion_pregunta.innerHTML = `
+        <div class= "preguntas-container">
+            <ul>
+                <li>${'Tema: ' + tema}</li> 
+                <li>${'Pregunta: ' + pregunta}</li> 
+            </ul>
+        </div> 
+        <div class= "buttons-container">
+            <button class= "disagree">Desacuerdo</button> 
+            <button class= "agree">De acuerdo</button>
+        </div>
+        `;
+        
         //El innerHtml remplaza lo que tenga previamente el elemento (body para este caso), por ende solo veran la lista de los ultimos del ciclo. Para hacer buen uso de eso les recomiendo utilizar los appends y no innerHtml, pero ya es el gusto de cada quien.
         document.getElementById("domBody").appendChild(seccion_pregunta);
     }
+
+
+    preguntasTema.reverse();
+
+    document.querySelectorAll('.buttons-container').forEach(((btn, index)=>{
+        var disagree = btn.querySelector('.disagree');
+        disagree.addEventListener('click', () => {
+            sumarD(index, preguntasTema[index]); 
+        });
+
+        var agree = btn.querySelector('.agree');
+        agree.addEventListener('click', () => {
+            sumarA(index, preguntasTema[index]);
+        });
+    }));
+
+    hacerSumatorias();
+
+    var seccion_resultado = document.createElement('section');
+    seccion_resultado.className = 'resultado';
+    seccion_resultado.innerHTML = `
+    <div class= "resultado-container">
+        <button class= "resultado">RESULTADO</button>
+    </div>
+    `;
+
+    document.getElementById("domBody").appendChild(seccion_resultado);
+
+    var resultado = document.querySelector('.resultado');
+        resultado.addEventListener('click', () => {
+            evaluacionAfinidad();
+
+            var textoR = document.createElement('h1');
+            textoR.innerHTML= `${recomendacion[0].nombreC}`;
+            seccion_resultado.appendChild(textoR);
+        });
 }
